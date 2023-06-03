48392d153833772f640e8bc993634e205c7c247e
eyusias <eyusiasa@gmail.com>
2023-06-02 19:28:31 -0700

    pet-park project committed for week-13

---
+++ src/main/java/pet/store/PetStoreApplication.java
@@ -0,0 +1,14 @@
+package pet.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreApplication {
+
+  public static void main(String[] args) {
+    // Starting spring boot application
+    SpringApplication.run(PetStoreApplication.class, args);
+  }
+
+}
