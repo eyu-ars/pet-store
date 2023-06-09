64a40aa944f0bd51972bea6cf325aef0989453cc
eyusias <eyusiasa@gmail.com>
2023-06-08 17:53:06 -0700

    Pet store app is modified for handling global exception, put and post requests

--- src/main/java/pet/store/PetStoreApplication.java
+++ src/main/java/pet/store/PetStoreApplication.java
@@ -3,6 +3,7 @@ package pet.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PetStoreApplication {
 
