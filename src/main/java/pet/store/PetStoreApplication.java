00400c9a7024d38709c5c3ef9ade4bbbc892fe70
eyusias <eyusiasa@gmail.com>
2023-06-15 17:57:21 -0700

    Pet store app is modified for saving customers and employees, retrieving and deleting pet store

--- src/main/java/pet/store/PetStoreApplication.java
+++ src/main/java/pet/store/PetStoreApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetStoreApplication {
 
   public static void main(String[] args) {
-    // Starting spring boot application
+    // Starting spring boot application 
     SpringApplication.run(PetStoreApplication.class, args);
   }
 
